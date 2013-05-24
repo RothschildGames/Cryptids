5ebd0253b84a8cc4fd6884fb4aa96cd2db80ed25
Shay Davidson <shay.h.davidson@gmail.com>
2013-05-24 23:55:36 +0300

    basic player added

---
+++ player/basic.rb
@@ -0,0 +1,17 @@
+module Player
+  class Basic < Abstract
+
+    def choose_aim
+      aim_cards.sample
+    end
+
+    def choose_action
+      action_cards.sample
+    end
+
+    def choose_another_action(other_than)
+      (action_cards.reject { |card| card == other_than } << nil).sample
+    end
+
+  end
+end
