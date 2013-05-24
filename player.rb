bc0e8bf3158d09ac3cea605825e1bea17db59b71
Shay Davidson <shay.h.davidson@gmail.com>
2013-05-24 17:37:21 +0300

    extracted to different files

---
+++ player.rb
@@ -0,0 +1,34 @@
+class Player
+  attr_accessor :energy, :race, :game, :aim_cards, :action_cards
+
+  def initialize
+    @energy = STARTING_ENERGY
+    @aim_cards = []
+    @action_cards = []
+  end
+
+  def create_hand
+    game.players.each do |other|
+      if other != self
+        aim_cards << AimCard.new(self, other)
+      end
+    end
+    ActionCard::TYPES.each do |type|
+      action_cards << ActionCard.new(self, type)
+    end
+    #race.extend_hand(self)
+  end
+
+  def choose_aim
+    aim_cards.sample
+  end
+
+  def choose_action
+    action_cards.sample
+  end
+
+end
+
+class Race
+
+end
--- simulator.rb
+++ simulator.rb
@@ -1,46 +1,9 @@
+require './card'
+require './player'
+
 NUMBER_OF_PLAYERS = 3
 STARTING_ENERGY = 5
 WINNING_ENERGY = 10
-
-class Race
-
-end
-
-class Card
-  attr_accessor :face_up, :owner
-
-  def initialize(owner)
-    @face_up = false
-    @owner = owner
-  end
-
-  def face_up!
-    @face_up = true
-  end
-
-  def face_up?
-    !!@face_up
-  end
-end
-
-class AimCard < Card
-  attr_accessor :target
-
-  def initialize(owner, target)
-    super(owner)
-    @target = target
-  end
-end
-
-class ActionCard < Card
-  attr_accessor :type
-  TYPES = [:attack, :block, :charge]
-
-  def initialize(owner, type)
-    super(owner)
-    @type = type
-  end
-end
 
 class Turn
   PHASES = [
@@ -84,39 +47,6 @@ class Game
     end
   end
 end
-
-class Player
-  attr_accessor :energy, :race, :game, :aim_cards, :action_cards
-
-  def initialize
-    @energy = STARTING_ENERGY
-    @aim_cards = []
-    @action_cards = []
-  end
-
-  def create_hand
-    game.players.each do |other|
-      if other != self
-        aim_cards << AimCard.new(self, other)
-      end
-    end
-    ActionCard::TYPES.each do |type|
-      action_cards << ActionCard.new(self, type)
-    end
-    #race.extend_hand(self)
-  end
-
-  def choose_aim
-    aim_cards.sample
-  end
-
-  def choose_action
-    action_cards.sample
-  end
-
-
-end
-
 
 game = Game.new()
 NUMBER_OF_PLAYERS.times { game.add_player Player.new }
