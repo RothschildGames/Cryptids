4e4f1f949b45f08a07cd2123031a9f0766934f5f
Shay Davidson <shay.h.davidson@gmail.com>
2013-05-31 14:40:49 +0300

    random players do not kill themselves

--- player/random.rb
+++ player/random.rb
@@ -10,6 +10,8 @@ module Player
     end
 
     def choose_another_action(excluded_action)
+      return :do_nothing if @energy == 1
+      
       actions_other_than(excluded_action).sample
     end
 
