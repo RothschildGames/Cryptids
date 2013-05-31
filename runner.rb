1e53d7fa8915baa31aad781ce355b91e408df2dc
yon <Lighthawky@gmail.com>
2013-05-31 14:37:50 +0300

    added game runner

---
+++ runner.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require './cryptids'
+
+case ARGV[0]
+  when /o(ptions)?/
+    run_multiple_options_games
+  when /m(ulti)?/
+    run_multiple_games
+  when /s(ingle)?/
+    run_single_game(:should_log => true)
+  else
+    puts 'OPTIONS:'
+    puts '[s]ingle - run a single game'
+    puts '[m]ulti - run 1000 games'
+    puts '[o]ptions - run a multivariant test of 1000 games each'
+end
