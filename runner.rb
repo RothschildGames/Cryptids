e4748db18784b923f778b5299c29d7604a68104a
Shay Davidson <shay.h.davidson@gmail.com>
2013-06-12 16:46:27 +0300

    started test

--- runner.rb
+++ runner.rb
@@ -9,9 +9,12 @@ case ARGV[0]
     run_multiple_games
   when /s(ingle)?/
     run_single_game(:should_log => true)
+  when /p(owers)?/
+    run_powers_balance_test
   else
     puts 'OPTIONS:'
     puts '[s]ingle - run a single game'
     puts '[m]ulti - run multiple games'
     puts '[o]ptions - run a multivariant test of 1000 games each'
+    puts '[p]powers - run a power test'
 end
